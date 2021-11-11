5670f1a9024a7f0f756ce4cdee155f94d8f8b74c
Antoniok2 <antoniopiazza18@gmail.com>
2021-11-11 15:29:22 +0100

    add real description slides text

--- js/main.js
+++ js/main.js
@@ -6,27 +6,27 @@ let carousel = new Vue({
             {
                 image: 'img/01.jpg',
                 title: 'Svezia',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                text: 'La Svezia posta nel cuore della Scandinavia, può essere considerata come una stupenda realtà nordica, resa caratteristica da meravigliosi paesaggi naturali, città incantevoli e ricche di cultura, posti selvaggi e pittoreschi. Per tutto questo e molto altro ancora la Svezia ogni anno attira un gran numero di turisti.',
             },
             {
                 image: 'img/02.jpg',
                 title: 'Svizzera',
-                text: 'Lorem ipsum',
+                text: 'Il suo territorio è quasi tutto montuoso. Si possono individuare tre grandi regioni: l-area alpina, nella parte meridionale del Paese; la fascia alpina degli altipiani, nella regione centrale; la catena del Giura, a occidente. Il settore alpino copre circa il 60% del territorio.',
             },
             {
                 image: 'img/03.jpg',
                 title: 'Gran Bretagna',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                text: 'Ogni angolo della Gran Bretagna nasconde un patrimonio artistico e culturale. E un paese antico, dalle grandi contraddizioni: le tradizioni si mescolano alla modernità e al dinamismo influenzando ancora il mondo intero con i propri stili di vita, la musica, la moda e le numerose opportunità per i giovani.',
             },
             {
                 image: 'img/04.jpg',
                 title: 'Germania',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam',
+                text: 'Nel cuore dell Europa, la Germania possiede meraviglie naturali veramente sorprendenti per bellezza e varietà dei paesaggi. Questi luoghi riusciranno ad affascinare tutti gli amanti della natura e anche i più esperti viaggiatori.',
             },
             {
                 image: 'img/05.jpg',
-                title: 'Paradise',
-                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam',
+                title: 'Maldive',
+                text: 'Le Maldive sono un magnifico arcipelago di isole e atolli situato nell’Oceano Indiano e rappresentano una delle mete turistiche più ambite e amate al mondo. Il loro incantevole clima e il loro paesaggio mozzafiato attirano infatti ogni anno un ingente flusso turistico. Gli splendidi atolli delle Maldive rappresentano il luogo ideale per rilassarsi e godersi il sole e il mare pulito e cristallino che lambisce alcune delle spiagge più belle al mondo.',
             },
         ],
     },
